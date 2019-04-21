d272ffbb40631222986b7bfcc949572764d6c6d5
Jiyun <cathydjy@gmail.com>
2019-04-21 18:40:31 -0400

    Update IBagOfWords.java

--- VocabularyApp/src/IBagOfWords.java
+++ VocabularyApp/src/IBagOfWords.java
@@ -1,4 +1,14 @@
+import java.util.Set;
 
 public interface IBagOfWords extends Event {
 	
+	
+	/**
+	 * 
+	 * @param bagSize - an integer indicating the size of the bag
+	 * @return Set<String> - a set that contains bagSize Words
+	 */
+	public Set<String> getBag(int bagSize);
+
 }
+
