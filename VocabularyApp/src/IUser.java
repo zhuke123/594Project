aa8ddddcf23110370276ddd504910399466f0060
ke zhu <zkwestlife@gmail.com>
2019-04-21 19:34:11 -0400

    user

--- VocabularyApp/src/IUser.java
+++ VocabularyApp/src/IUser.java
@@ -1,4 +1,23 @@
 
 public interface IUser {
-
+	
+	/**
+	 * @return count of word known
+	 */
+	public int getWordCount();
+	
+	/**
+	 * @return username
+	 */
+	public String getUsername();
+	
+	/**
+	 * flashcard event
+	 */
+	public void flashcard();
+	
+	/**
+	 * bag of words event
+	 */
+	public void bagOfWords();
 }
