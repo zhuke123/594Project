9b7d70db57ffd94b0b76301419a1fba7757acb14
ke zhu <zkwestlife@gmail.com>
2019-04-21 17:59:19 -0400

    dictionary
    dictionary, program

--- VocabularyApp/src/IProgram.java
+++ VocabularyApp/src/IProgram.java
@@ -2,8 +2,20 @@ import java.util.*;
 
 public interface IProgram {
 	
-
+	/**
+	 * @return list of users
+	 * */
+	public List<IUser> getDict();
 	
+	/**
+	 * 
+	 * @return ranking system
+	 */
+	public IRank getRank();
 	
+	/**
+	 * initiate the program
+	 */
+	public void init();
 
 }
