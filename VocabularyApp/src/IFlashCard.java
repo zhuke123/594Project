95f1ea30bfe4871d4ba7bdff2d0847471b99786d
kengpian <kengpian@seas.upenn.edu>
2019-04-21 19:05:34 -0400

    Update IFlashCard.java

--- VocabularyApp/src/IFlashCard.java
+++ VocabularyApp/src/IFlashCard.java
@@ -7,4 +7,10 @@ public interface IFlashCard extends Event {
 	 * return the top word of the user's heap
 	 */
 	public IWord getFlashCard();
+	
+	/***
+	 * 
+	 * insert the updated word back into the heap
+	 */
+    public void insert(IWord word);
 }
