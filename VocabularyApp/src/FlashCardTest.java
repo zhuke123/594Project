57651f2180e463ab4905a154ba7145b951400e41
kengpian <kengpian@seas.upenn.edu>
2019-04-23 21:40:19 -0400

    Update FlashCardTest.java

--- VocabularyApp/src/FlashCardTest.java
+++ VocabularyApp/src/FlashCardTest.java
@@ -14,14 +14,14 @@ public class FlashCardTest {
 
 	@Test
 	public void testCreateDataStructure() {
-		Word word1 = new Word("a",1);
-		Word word2 = new Word("b",2);
-		Word word3 = new Word("c",3);
-		Word word4 = new Word("d",4);
-		Word word5 = new Word("e",5);
-		Word word6 = new Word("f",6);
-		Word word7 = new Word("g",7);
-		Word word8 = new Word("h",8);
+		Word word1 = new Word("a","def",1);
+		Word word2 = new Word("b","def",2);
+		Word word3 = new Word("c","def",3);
+		Word word4 = new Word("d","def",4);
+		Word word5 = new Word("e","def",5);
+		Word word6 = new Word("f","def",6);
+		Word word7 = new Word("g","def",7);
+		Word word8 = new Word("h","def",8);
 		ArrayList<IWord> wordList = new ArrayList<IWord>();
 		wordList.add(word1);
 		wordList.add(word2);
@@ -33,12 +33,19 @@ public class FlashCardTest {
 		wordList.add(word8);
 		FlashCard fc = new FlashCard();
 		fc.createDataStructure(wordList);
-		assertEquals(fc.getFlashCard(),word8);
+		assertEquals(fc.getFlashCard().getWord(),word8.getWord());
 		assertEquals(fc.getFlashCard(),word7);
 		word8.updateWeight(9.2);
 		fc.insert(word8);
 		assertEquals(fc.getFlashCard(),word8);
-		
 	}
+	
+	@Test(expected = NullPointerException.class)
+		public void testNull(){
+		FlashCard fc = new FlashCard();
+		ArrayList<IWord> wordList = null;
+		fc.createDataStructure(wordList);
+	}
+	
 	
 }
