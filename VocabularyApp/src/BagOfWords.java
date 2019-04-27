3b145f37b0eb5f4ecc9e25871c7f545e26d25789
Jiyun <cathydjy@gmail.com>
2019-04-26 21:59:59 -0400

    bagofwords

--- VocabularyApp/src/BagOfWords.java
+++ VocabularyApp/src/BagOfWords.java
@@ -32,7 +32,33 @@ public class BagOfWords implements IBagOfWords{
 
 	@Override
 	public Set<String> getBag(int bagSize) {
-		return null;
+		Set<String> bag = new HashSet<String>();
+		HashMap<HashSet<Integer>, Integer> permunation= new HashMap<HashSet<Integer>,Integer>();
+		Set<HashSet<IWord>> key = new HashSet<HashSet<IWord>>();
+		HashSet<IWord> currentSet = new HashSet<IWord>();
+		generateBag(setOfWord,bagSize,key,currentSet);
+		return bag;
 	}
-
+	
+	public void generateBag(Set<IWord> setOfWords,int bagSize,Set<HashSet<IWord>> combination,HashSet<IWord> currentSet) {
+ 		if(bagSize>currentSet.size()) {
+			for(IWord w:setOfWords) {
+				System.out.print("size:"+ setOfWords.size()+ "\n");
+				//System.out.print(w.getWord()+ "\n");
+				currentSet.add(w);
+			    Set<IWord> tmp = new HashSet<IWord>(setOfWords);
+			    tmp.remove(w);
+			    for(IWord i: currentSet) {
+			    	System.out.print(i.getWord()+" ");
+			    }
+				generateBag(tmp,bagSize,combination,currentSet);
+			}
+		}
+		else {
+			combination.add(new HashSet<>(currentSet));
+			currentSet.clear();
+			generateBag(setOfWords,bagSize,combination,currentSet);
+		}
+	}
+ 
 }
