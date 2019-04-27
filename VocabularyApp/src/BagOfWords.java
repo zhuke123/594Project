6ee681922d492f72747976382f689f7e64c19618
Jiyun <cathydjy@gmail.com>
2019-04-27 13:11:55 -0400

    bagofword

--- VocabularyApp/src/BagOfWords.java
+++ VocabularyApp/src/BagOfWords.java
@@ -32,26 +32,34 @@ public class BagOfWords implements IBagOfWords{
 
 	@Override
 	public Set<Word> getBag(int bagSize) {
-		Set<String> bag = new HashSet<String>();
+		
 		Set<HashSet<IWord>> key = new HashSet<HashSet<IWord>>();
 		HashSet<IWord> currentSet = new HashSet<IWord>();
 		HashMap<HashSet<IWord>, Integer> permutation= new HashMap<HashSet<IWord>,Integer>();
 		generateBag(setOfWord,bagSize,key,currentSet);
+		System.out.print(key.size()+"\n");
+		//get common text - intersection Word's text source
 		for(HashSet<IWord> s: key) {
 			List<HashSet<Integer>> textlist = new ArrayList<HashSet<Integer>>();
 			for(IWord w:s) {
-				textlist.add((HashSet<Integer>) w.getTextSources());
+				System.out.print(w.getWord()+":"+w.getTextSources().size());
+				if(w.getTextSources()!=null) {
+					textlist.add((HashSet<Integer>) w.getTextSources());
+				}
 			}
+			System.out.print("\n");
 			HashSet<Integer> commonText = textlist.get(0);
 			for(int i = 1; i < textlist.size();i++) {
 				commonText.retainAll(textlist.get(i));
 			}
 			permutation.put(s,commonText.size());
+			System.out.print(permutation.size()+"\n");
 		}
-		
-		ArrayList max = new ArrayList<Integer>(permutation.values());
+		ArrayList<Integer> max = new ArrayList<Integer>(permutation.values());		
 		Collections.sort(max,Collections.reverseOrder());
+		//System.out.print(max.get(0));
 		for(HashSet<IWord> k: permutation.keySet()) {
+			//System.out.print(k.size());
 			if(permutation.get(k)==max.get(0)) {
 				for(IWord w: k) {
 					bagOfWord.add((Word) w);
@@ -61,7 +69,8 @@ public class BagOfWords implements IBagOfWords{
 		return bagOfWord;
 	}
 	
-	public void generateBag(Set<IWord> setOfWords,int bagSize,Set<HashSet<IWord>> combination,HashSet<IWord> currentSet) {
+	
+	private void generateBag(Set<IWord> setOfWords,int bagSize,Set<HashSet<IWord>> combination,HashSet<IWord> currentSet) {
  		if(bagSize>currentSet.size()) {
 			for(IWord w:setOfWords) {
 				currentSet.add(w);
